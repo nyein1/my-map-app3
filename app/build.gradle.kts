6c9b0762135efbd11464f2af58d6ee354149f121
nyein1 <88401590+nyein1@users.noreply.github.com>
2026-09-21 16:06:17 +0630

    Add app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.nyein1.mymapapp3"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.nyein1.mymapapp3"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.gms:play-services-maps:19.0.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+}
