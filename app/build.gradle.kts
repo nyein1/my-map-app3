a3a3eae40552573438657fc71029bbbfd7cbc42c
nyein1 <88401590+nyein1@users.noreply.github.com>
2026-09-22 01:54:53 +0630

    Configure fuel calculator Android app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,6 +11,9 @@ android {
         applicationId = "com.nyein1.mymapapp3"
         minSdk = 23
         targetSdk = 35
+        versionCode = 2
+        versionName = "2.0"
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -20,15 +23,9 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
-        versionCode = 1
-        versionName = "1.0"
-    }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("com.google.android.gms:play-services-maps:19.0.0")
-    implementation("com.google.android.gms:play-services-location:21.3.0")
 }
